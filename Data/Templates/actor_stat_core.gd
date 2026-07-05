474af8665e480f27f8e97fffcddef2511fb7a6fa
NE_Neighbor <newenglandneighbor@gmail.com>
2026-07-05 17:33:03 -0400

    Delete Data directory
    folder restructure

--- Data/Templates/actor_stat_core.gd
+++
@@ -1,8 +0,0 @@
-class_name ActorStats extends Resource
-
-@export var actor_name : String = "Default"
-@export var actor_attributes : Dictionary[String,Variant]
-
-func get_attribute_by_key(query:String) -> Variant:
-	if !actor_attributes.has(query): return null
-	return actor_attributes[query]

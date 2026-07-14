0a5808f55babb6d5b8230b83ea374694380d05d8
NE_Neighbor <newenglandneighbor@gmail.com>
2026-07-14 13:12:38 -0400

    Update math_plus.gd
    added exponential decay function for floating point values.

--- common/math_plus.gd
+++ common/math_plus.gd
@@ -3,3 +3,6 @@ class_name MathPlus
 const DECAY : float = 16.0
 static func v3_exp_decay(a:Vector3,b:Vector3,decay:float,time:float) -> Vector3:
 	return Vector3(b+(a-b)*exp(-decay*time))
+
+static func f_exp_decay(a:float,b:float,decay:float,time:float) -> float:
+	return b + (a - b) * exp(-decay * time)

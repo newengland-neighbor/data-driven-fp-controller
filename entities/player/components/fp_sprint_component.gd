76151bed08bb75a99afa57f8ceec199dc9505c85
NE_Neighbor <newenglandneighbor@gmail.com>
2026-07-14 13:16:01 -0400

    Update fp_sprint_component.gd
    added option for omni-directional sprinting, as well as various documentation updates

--- entities/player/components/fp_sprint_component.gd
+++ entities/player/components/fp_sprint_component.gd
@@ -1,6 +1,10 @@
 class_name FPSprintComponent extends ComponentCore
 
+##Modifier value used to augment FPController's movement speed.
 @export_range(1.0, 2.0, 0.05) var speed_modifier : float = 2.0
+##If true, the player will be able to sprint in all horizontal directions.
+##If false, the player will only be able to sprint if moving forward.
+@export var omni_directional : bool = true
 
 var character_body_ref : FPController
 signal send_move_speed_mod(key:String,val:float,remove:bool)
@@ -24,9 +28,13 @@ func update(_delta:float) -> void:
 		return
 	
 	if Input.is_action_pressed("sprint"):
-		var check : bool = (
-			character_body_ref.has_modifier("crouch")
-			or character_body_ref.has_modifier("sprint")
-		)
-		if check: return
+		# If player is crouching, exit function.
+		var check : Variant = character_body_ref.has_modifier("crouch")
+		if check != null: return
+		# If omni-directional sprint isn't allowed, check for forward movement
+		# before sending speed modifier off.
+		if !omni_directional and !Input.is_action_pressed("move_forward"): 
+			if character_body_ref.has_modifier("sprint"): 
+				send_move_speed_mod.emit("sprint",true,speed_modifier)
+			return
 		send_move_speed_mod.emit("sprint",false,speed_modifier)

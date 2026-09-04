1be491e71730a2642bd473e5da72332221c444d0
NE_Neighbor <newenglandneighbor@gmail.com>
2026-09-04 18:01:57 -0400

    Delete systems directory

--- systems/audio_library_player/audio_library.gd
+++
@@ -1,7 +0,0 @@
-class_name AudioLibrary extends Resource
-
-@export var streams : Dictionary[String,AudioStream]
- 
-func get_sound_by_key(key:String) -> AudioStream:
-	if !streams.has(key): return null
-	return streams[key]

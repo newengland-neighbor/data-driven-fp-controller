17f04bc7b89ed742533a80debf662df6d1b87642
NE_Neighbor <newenglandneighbor@gmail.com>
2026-09-04 18:19:02 -0400

    Update ground_cast_3d.gd

--- entities/player/ground_cast_3d.gd
+++ entities/player/ground_cast_3d.gd
@@ -1,30 +1,9 @@
 class_name GroundCast3D extends RayCast3D
 
-@warning_ignore("unused_signal")
-signal send_message_to_router(msg:Message)
-
-func _ready() -> void:
-	var msg_id : int = -1
-	# If generated ID exists in the message router's registry, keep generating a new ID until
-	# a free spot for that key is found.
-	while (MessageRouter.registry_has_key(msg_id) or msg_id == -1): msg_id = randi()
-	MessageRouter.register_client(msg_id, handle_message)
+func _ready() -> void: pass
 
 func _physics_process(_delta: float) -> void:
 	if !self.is_colliding(): return
-	#var collision_msg : Message = Message.new(
-		#self.get_meta("msg_id",-1),
-		#get_parent().get_meta("msg_id",-1),
-		#Message.MessageTypes.SEND_DATA,
-		#{"collision_data":get_collision_data()}
-		#)
-	#var sound_msg : Message = Message.new(
-		#self.get_meta("msg_id",-1),
-		# eventual Footstep Sound Manager?
-		# send_data
-		#null if !col_ref.has_meta("material") else col_ref.get_meta("material")
-		#)
-	
 
 func get_collision_data() -> Dictionary:
 	var output : Dictionary = {}
@@ -32,5 +11,3 @@ func get_collision_data() -> Dictionary:
 	output["normal"] = self.get_collision_normal()
 	output["slope"] = Vector3.UP.angle_to(self.get_collision_normal())
 	return output
-
-func handle_message() -> void: pass

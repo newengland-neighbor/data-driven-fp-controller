79c8c93f045c768c3db497870841106358d3a6a8
NE_Neighbor <newenglandneighbor@gmail.com>
2026-09-04 18:17:31 -0400

    Delete systems/interaction directory

--- systems/interaction/door/states/door_state.gd
+++
@@ -1,8 +0,0 @@
-class_name DoorState extends StateResource
-
-enum DoorStates {CLOSED, OPEN}
-
-func bind(new:ComponentCore,indx:int) -> void:
-	super.bind(new,indx)
-
-func on_input_received() -> void: pass

96c1c9d930a91c00adbaba83f92e7951d093107d
NE_Neighbor <newenglandneighbor@gmail.com>
2026-09-15 18:54:08 -0400

    Update fp_on_ground.gd

--- entities/player/states/fp_on_ground.gd
+++ entities/player/states/fp_on_ground.gd
@@ -4,7 +4,7 @@ class_name FPMS_OnGround extends FPMoveState
 @export var coyote_timer_max : float = 0.15
 
 var coyote_timer : float = 0.0
-var wish_dir : Vector3 = Vector3.ZERO
+var lerped_velo : Vector3 = Vector3.ZERO
 
 var character_body_ref : FPController
 
@@ -15,33 +15,35 @@ func bind(new:ComponentCore,indx:int) -> void:
 func enter(_prev_state:int,_data:={}) -> void: 
 	if !_data.is_empty():
 		var prev_velocity = _data["prev_velocity"]
-		wish_dir = prev_velocity
-	else: wish_dir = Vector3.ZERO
+		lerped_velo = prev_velocity
+	else: lerped_velo = Vector3.ZERO
 	coyote_timer = 0.0
-	#print("Entering on_ground state...")
 
 func handle_input(_event:InputEvent) -> void:
 	if _event .is_action_pressed("jump"):
 		if !character_body_ref.ceiling_check(0.5): return
 		finished.emit(MoveStates.IN_AIR, {
-			"prev_velocity":wish_dir,
+			"prev_velocity":lerped_velo,
 			"has_jumped":true
 			})
 
 func update(_delta:float) -> void: pass
 
 func physics_update(_delta:float) -> void: 
-	var target_vector : Vector3 = (
-		character_body_ref.get_desired_direction()
-		if character_body_ref.get_input() != Vector2.ZERO
+	var wish_dir : Vector3 = (
+		character_body_ref.get_desired_direction() 
+		if character_body_ref.get_input() != Vector2.ZERO 
 		else Vector3.ZERO )
 	
-	wish_dir = MathPlus.v3_exp_decay(wish_dir,target_vector,MathPlus.DECAY,_delta)
+	if character_body_ref.get_desired_direction().length() > 0:
+		lerped_velo = MathPlus.v3_exp_decay(lerped_velo,wish_dir,move_acceleration,_delta)
+	else: lerped_velo = MathPlus.v3_exp_decay(lerped_velo,wish_dir,move_drag,_delta)
+	
 	var was_on_floor : bool = character_body_ref.is_on_floor()
 	
 	var has_snapped : bool = character_body_ref.handle_step_up(_delta)
 	
-	character_body_ref.velocity = wish_dir * character_body_ref.get_total_speed() * Vector3(1,0,1)
+	character_body_ref.velocity = lerped_velo * character_body_ref.get_total_speed() * Vector3(1,0,1)
 	character_body_ref.handle_physics_collision()
 	character_body_ref.move_and_slide()
 	
@@ -61,8 +63,6 @@ func physics_update(_delta:float) -> void:
 				"has_jumped":false
 				})
 
-func exit() -> void: 
-	pass
-	#print("Exiting on_ground state...")
+func exit() -> void: pass
 
 func _in_coyote_time() -> bool: return coyote_timer < coyote_timer_max

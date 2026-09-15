6454a711bf0a83e1a5881e8f56c712ea7cca0d18
NE_Neighbor <newenglandneighbor@gmail.com>
2026-09-15 18:52:52 -0400

    Update fps_move_state.gd
    added variables used for improved locomotion method.

--- entities/player/states/fps_move_state.gd
+++ entities/player/states/fps_move_state.gd
@@ -1,5 +1,10 @@
 class_name FPMoveState extends StateResource
 
+##Value used to control how fast the player's velocity interpolates to the desired movement direction.
+@export var move_acceleration : float = 1.0
+##Value used to control the speed at which the player stops moving once no input is being registered.
+@export var move_drag : float = 1.0
+
 enum MoveStates {
 	ON_GROUND,
 	IN_AIR,

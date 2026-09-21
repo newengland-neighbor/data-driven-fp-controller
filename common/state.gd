35b038450a1c79191527c1e311e12c36b9a207d9
NE_Neighbor <newenglandneighbor@gmail.com>
2026-09-21 11:47:30 -0400

    Update state.gd
    Removed the locally-stored state index (which was made redundant by changes to StateMachineComponent).

--- common/state.gd
+++ common/state.gd
@@ -1,19 +1,15 @@
 class_name StateResource extends Resource
 
 var state_owner : StateMachineComponent
-var index : int
 
 signal finished(new_state:int,data:Dictionary)
 
-func enter(_prev_state:int,_data:={}) -> void: pass
+func enter(_prev_state:String,_data:={}) -> void: pass
 func handle_input(_event:InputEvent) -> void: pass
 func update(_delta:float) -> void: pass
 func physics_update(_delta:float) -> void: pass
 func exit() -> void: pass
 
-func bind(new:StateMachineComponent,indx:int) -> void:
+func bind(new:StateMachineComponent) -> void:
 	finished.connect(new.on_state_transition)
 	state_owner = new
-	index = indx
-
-func get_index() -> int: return index
